a9ca0ece1b7d60c3af24898254a7379be767c615
Adexion <mateusz.pozniak.93@gmail.com>
2021-12-03 19:48:13 +0100

    add new yarn packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const Encore = require('@symfony/webpack-encore');
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
@@ -16,11 +18,9 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
-
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = 3;
@@ -29,6 +29,26 @@ Encore
     .enableTypeScriptLoader()
     .enableIntegrityHashes(Encore.isProduction())
     .autoProvidejQuery()
+    .addPlugin( new CompressionPlugin({
+        filename: "[path][base].gz",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+    }))
+    .addPlugin(new CompressionPlugin({
+        filename: "[path][base].br",
+        algorithm: "brotliCompress",
+        test: /\.(js|css|html|svg)$/,
+        compressionOptions: {
+            params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+            },
+        },
+        threshold: 10240,
+        minRatio: 0.8,
+        deleteOriginalAssets: false,
+    }))
 ;
 
 module.exports = Encore.getWebpackConfig();
