988693294ba1063f6d99063cd8dd90da9430c8df
Adexion <mateusz.pozniak.93@gmail.com>
2021-12-03 18:52:02 +0100

    repair

--- src/Controller/Admin/Crud/ItemCrud.php
+++ src/Controller/Admin/Crud/ItemCrud.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin\Crud;
 
 use App\Controller\Admin\Field\CKEditorField;
 use App\Entity\Item;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
@@ -20,6 +21,17 @@ class ItemCrud extends AbstractCrudController
         return Item::class;
     }
 
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param Item $entityInstance
+     */
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityInstance->setPrice(round($entityInstance->getPrice(), 2));
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -34,7 +46,7 @@ class ItemCrud extends AbstractCrudController
                 ->setCurrency('PLN')
                 ->setNumDecimals(2)
                 ->setStoredAsCents(false)
-                ->setCustomOption('html5', true),
+                ->setFormTypeOption('html5', true),
             CollectionField::new('command')
                 ->allowAdd(true)
                 ->allowDelete(true),
