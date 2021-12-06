1ee1fea7067e200372a6afed814de3d2ef7a578d
Adexion <mateusz.pozniak.93@gmail.com>
2021-12-06 09:03:43 +0100

    changes in deploying script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 git pull
-bin/console cache:clear
+composer dump-autoload --no-dev --classmap-authoritative
 php bin/console doctrine:schema:update --force
 composer install
-composer dump-autoload --no-dev --classmap-authoritative
 yarn build
