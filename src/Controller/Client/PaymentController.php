82eb3d89550936a7b693618f1af26284149a845d
Adexion <mateusz.pozniak.93@gmail.com>
2021-12-06 11:01:06 +0100

    test for sfite

--- src/Controller/Client/PaymentController.php
+++ src/Controller/Client/PaymentController.php
@@ -27,7 +27,6 @@ class PaymentController extends AbstractController
      */
     public function status(Request $request, PaymentExecutionService $executionService): Response
     {
-        var_dump($request->request->all());die;
         $form = $this->createForm(PaymentStatusType::class);
         $form->handleRequest($request);
 
