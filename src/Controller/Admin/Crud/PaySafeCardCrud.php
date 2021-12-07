d600a1e5b44054a5feee3959ceae90550eab046c
Adexion <mateusz.pozniak.93@gmail.com>
2021-12-07 18:27:40 +0100

    add informations how it works

--- src/Controller/Admin/Crud/PaySafeCardCrud.php
+++ src/Controller/Admin/Crud/PaySafeCardCrud.php
@@ -5,21 +5,35 @@ namespace App\Controller\Admin\Crud;
 use App\Controller\Admin\Field\EntityField;
 use App\Entity\Item;
 use App\Entity\PaySafeCard;
-use App\Entity\Voucher;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PaySafeCardCrud extends AbstractCrudController
 {
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public static function getEntityFqcn(): string
     {
         return PaySafeCard::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setHelp(Crud::PAGE_INDEX,
+                $this->translator->trans('PaySafeCard used flag when active it is allow to generating voucher for user')
+            );
     }
 
     public function configureActions(Actions $actions): Actions
@@ -39,7 +53,7 @@ class PaySafeCardCrud extends AbstractCrudController
             DateField::new('date', 'Date From')
                 ->renderAsNativeWidget(true)
                 ->setDisabled(),
-            BooleanField::new('used')
+            BooleanField::new('used'),
         ];
     }
 }
--- src/Controller/Admin/Crud/PaySafeCardVoucherCrud.php
+++ src/Controller/Admin/Crud/PaySafeCardVoucherCrud.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin\Crud;
 
 use App\Controller\Admin\Field\EntityField;
-use App\Entity\Item;
 use App\Entity\PaySafeCard;
 use App\Entity\PaySafeCardVoucher;
 use App\Entity\Voucher;
@@ -11,16 +10,29 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PaySafeCardVoucherCrud extends AbstractCrudController
 {
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public static function getEntityFqcn(): string
     {
         return PaySafeCardVoucher::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setHelp(Crud::PAGE_INDEX,
+                $this->translator->trans('PaySafeCard used flag when active it is allow to generating voucher for user')
+            );
     }
 
     public function configureActions(Actions $actions): Actions
