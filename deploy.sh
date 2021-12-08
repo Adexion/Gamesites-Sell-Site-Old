307345d63171899be88631d32625347330b5fb06
root <root@CT1257.hostgier.pl>
2021-12-08 10:19:41 +0000

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@ git pull
 composer dump-autoload --no-dev --classmap-authoritative
 php bin/console doctrine:schema:update --force
 composer install
+composer dump-autoload --no-dev --classmap-authoritative
+php bin/console doctrine:schema:update --force
+composer install
 yarn build
