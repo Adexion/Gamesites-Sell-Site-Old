eda8cd6916238cffb2df5e42b82fcc0086f93a32
root <root@Adexion-Komp.localdomain>
2022-02-17 10:27:23 +0100

    add removing cache to script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 git pull
 composer dump-autoload --no-dev --classmap-authoritative
+rm var/cache -R
 php bin/console doctrine:schema:update --force
 composer install
 composer dump-autoload --no-dev --classmap-authoritative
