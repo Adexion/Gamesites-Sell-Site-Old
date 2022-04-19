5f5418462633f8f4f80c85bae2bd84e560028c43
Adexion <mateusz.pozniak.93@gmail.com>
2022-04-19 14:27:52 +0200

    repair purple template

--- src/Controller/Admin/Crud/UserCrud.php
+++ src/Controller/Admin/Crud/UserCrud.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin\Crud;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -58,16 +59,23 @@ class UserCrud extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            TextField::new('nick')
+                ->setFormTypeOption('constraints', [new Length(["min" => 3, "max" => 25])]),
             EmailField::new('email')
+                ->setDisabled()
                 ->setFormTypeOption('constraints', [
                     new Email(['mode' => Email::VALIDATION_MODE_STRICT]),
                     new Length(['max' => 180]),
                 ]),
             TextField::new('password')
-                ->setFormType(PasswordType::class)
-                ->setFormTypeOption('constraints', [new Length(['min' => 8, 'max' => 255])]),
-            TextField::new('nick')
-                ->setFormTypeOption('constraints', [new Length(["min" => 3, "max" => 25])]),
+                ->setFormType(RepeatedType::class)
+                ->setFormTypeOptions([
+                    'type' => PasswordType::class,
+                    'first_options'  => ['label' => 'Password'],
+                    'second_options' => ['label' => 'Repeat password'],
+                ])
+                ->setFormTypeOption('constraints', [new Length(['min' => 8, 'max' => 255])])
+                ->hideOnIndex(),
         ];
     }
 }
