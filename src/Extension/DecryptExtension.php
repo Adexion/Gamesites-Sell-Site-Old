448923fd830341a0bb7b699375ec5338916a90b0
Adexion <mateusz.pozniak.93@gmail.com>
2022-05-21 19:00:35 +0200

    better shop site

--- src/Extension/DecryptExtension.php
+++ src/Extension/DecryptExtension.php
@@ -16,7 +16,7 @@ class DecryptExtension extends AbstractExtension
 
     public function mcryptEncrypt(string $key, string $text): string
     {
-        list($encrypted_data, $iv) = explode('::', base64_decode($text), 2);
+        [$encrypted_data, $iv] = explode('::', base64_decode($text), 2);
 
         return openssl_decrypt($encrypted_data, 'aes-256-cbc', $key, 0, $iv);
     }
---
+++ src/Extension/FileExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Extension;
+
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class FileExtension extends AbstractExtension
+{
+    private KernelInterface $appKernel;
+
+    public function __construct(KernelInterface $appKernel)
+    {
+        $this->appKernel = $appKernel;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('imageExist', [$this, 'remoteFileExists']),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('imageExist', [$this, 'remoteFileExists'])
+        ];
+    }
+
+    public function remoteFileExists(string $image): bool
+    {
+        $path = $this->appKernel->getProjectDir() . '/public/assets/images/';
+        $content = @file_exists($path . $image);
+
+        return $content !== false;
+    }
+}
