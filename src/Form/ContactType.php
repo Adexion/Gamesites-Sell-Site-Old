8ca9e001642699f35ae01140207dd22a339adf36
Mateusz Poźniak <m.pozniak@globkurier.pl>
2023-11-07 18:46:08 +0100

    rep captcha

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContactType extends BaseType
@@ -26,7 +28,16 @@ class ContactType extends BaseType
             ->add('captcha', CaptchaType::class)
             ->add('name', TextType::class)
             ->add('email', EmailType::class)
-            ->add('submit', SubmitType::class);
+            ->add('submit', SubmitType::class)
+            ->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'preSubmit']);
+    }
+
+    public function preSubmit(FormEvent $event)
+    {
+        $data = $event->getData();
+
+        unset($data['captcha']);
+        $event->setData($data);
     }
 
     public function configureOptions(OptionsResolver $resolver)
